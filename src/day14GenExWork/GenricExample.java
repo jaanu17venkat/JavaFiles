d65c55a5378de6841d1e04c09e1f1534a51566d8
jaanu17venkat <jaanu17venkat@gmail.com>
2020-06-15 07:26:23 +0530

    day 14 assignment

---
+++ src/day14GenExWork/GenricExample.java
@@ -0,0 +1,17 @@
+package day14GenExWork;
+
+import java.util.Stack;
+
+public class GenricExample {
+
+	void stackExample() {
+		Stack<Integer> intStack = new Stack<Integer>();
+		intStack.push(100);
+		intStack.push(213);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
